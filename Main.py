ba6fd65d40a79153b2166d725b1ae604a3f38239
unknown <laimonis20100@student.hamk.fi>
2021-04-23 10:50:19 +0300

    add  analog clock

--- Main.py
+++ Main.py
@@ -47,7 +47,50 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         
+        self.lbl = tk.Label(self, bg="white")
+        self.lbl.place(x=10, y=10, height=200, width=200)
         
+        self.working()
+        
+        
+    def clock_image(self, hr, min_, sec_):
+        clock=Image.new("RGB",(400, 400), (255, 255, 255))
+        draw = ImageDraw.Draw(clock)
+        # For clock image
+        bg = Image.open("clock4.png")
+        bg=bg.resize((200,200), Image.ANTIALIAS)
+        clock.paste(bg, (100, 100))
+        
+       
+        # Hour Line Image
+        origin = 200, 200
+        draw.line((origin, 200+50*sin(radians(hr)), 200-50*cos(radians(hr))), fill="black", width=4)
+        # Min Line Image
+        draw.line((origin, 200+80*sin(radians(min_)), 200-80*cos(radians(min_))), fill="black", width=4)
+        # Sec Line Image
+        draw.line((origin, 200+80*sin(radians(sec_)), 200-80*cos(radians(sec_))), fill="black", width=1)
+        
+        draw.ellipse((195, 195, 210, 210), fill="black")
+        
+        
+        
+        
+        clock.save("clock_new.png")
+        
+        
+    def working(self):
+        h=datetime.now().time().hour
+        m=datetime.now().time().minute
+        s=datetime.now().time().second
+        
+        hr = (h/12)*360
+        min_=(m/60)*360
+        sec_=(s/60)*360
+        
+        self.clock_image(hr,min_,sec_)
+        self.img = ImageTk.PhotoImage(file="clock_new.png")
+        self.lbl.config(image=self.img)
+        self.lbl.after(200, self.working)
         
         
         
